8dadadfbb775cadcf946f4d386988a8edf7bfc30
Angelo <agiavio@gmail.com>
2025-03-18 01:44:43 -0300

    añade funcion agregarAmigo

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+
+function agregarAmigo() {
+  let ul = document.getElementById('listaAmigos')
+  ul.innerHTML = ''
+
+  const amigo = document.getElementById('amigo').value
+
+  if (amigo === '') {
+    alert('Nombre no puede estar vacio')
+  } else {
+    amigos.push(amigo)
+    console.log(amigos)
+    document.getElementById('amigo').value = ''
+
+    amigos.forEach(a => {
+      let li = document.createElement('li')
+      li.textContent = a
+      ul.appendChild(li)
+    })
+  }
+}
