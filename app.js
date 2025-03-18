d28dfe0ea061a4969b080ea2244b26c04cb1a309
Angelo <agiavio@gmail.com>
2025-03-18 01:45:53 -0300

    se añade funcion sortearAmigo

--- app.js
+++ app.js
@@ -21,3 +21,23 @@ function agregarAmigo() {
     })
   }
 }
+
+function sortearAmigo() {
+  let ul = document.getElementById('resultado')
+  let ulAmigos = document.getElementById('listaAmigos')
+  ul.innerHTML = ''
+
+  if (amigos.length === 0) {
+    alert('No hay amigos en la lista')
+  } else {
+
+    let indice = Math.floor(Math.random() * amigos.length)
+    let amigoSeleccionado = amigos[indice]
+    console.log(amigoSeleccionado)
+
+    let li = document.createElement('li')
+    li.textContent = 'El amigo secreto sorteado es: ' + amigoSeleccionado
+    ul.appendChild(li)
+    ulAmigos.innerHTML = ''
+  }
+}
